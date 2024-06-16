fa091e9aef66498e34ebd5ebaf815d458249c151
Евгений Кучма <desferto87@gmail.com>
2024-06-16 21:04:39 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homeWorkFourRearrangementOfValues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fistName = "Петров";
+            string lastName = "Олег";
+
+            Console.WriteLine($"{fistName} {lastName}");
+
+            string transitVariable = fistName;
+
+            fistName = lastName;
+            lastName = transitVariable;
+
+            Console.WriteLine($"{fistName} {lastName}");
+        }
+    }
+}
